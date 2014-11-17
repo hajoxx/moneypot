8853c42200793cfedc111b4aaee3a9d21360589e
Ryan Havar <ryan@moneypot.com>
2014-11-17 13:45:50 +0000

    Use replyTo for contact emails

--- server/sendEmail.js
+++ server/sendEmail.js
@@ -22,7 +22,8 @@ exports.contact = function(from, content, user, callback) {
 
     var details = {
         to: 'ryan@moneypot.com',
-        from: from,
+        from: 'contact@moneypot.com',
+        replyTo: from,
         subject: 'Moneypot Contact (' + from + ')',
         html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
             '<html xmlns="http://www.w3.org/1999/xhtml">' +
