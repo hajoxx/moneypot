b4448e36989278b3d58b9d96d075af1371436eb3
Ryan Havar <ryan@moneypot.com>
2015-01-26 16:16:53 +0000

    Fix broken schema (issue #139)

--- server/schema.sql
+++ server/schema.sql
@@ -252,30 +252,6 @@ CREATE TABLE game_hashes
 );
 
 
-CREATE MATERIALIZED VIEW leaderboard AS
- WITH t AS (
-         SELECT u.id AS user_id,
-            u.username,
-            (COALESCE(sum(p.cash_out - p.bet), 0::numeric) + COALESCE(sum(p.bonus), 0::numeric))::bigint AS gross_profit,
-            (COALESCE(sum(p.cash_out), 0::numeric) + COALESCE(sum(p.bonus), 0::numeric) - COALESCE(sum(p.bet), 0::numeric))::bigint AS net_profit,
-            count(*) AS games_played
-           FROM plays p,
-            games g,
-            users u
-          WHERE p.game_id = g.id AND p.user_id = u.id AND p.id IS NOT NULL
-          GROUP BY u.id
-        )
- SELECT t.user_id,
-    t.username,
-    t.gross_profit,
-    t.net_profit,
-    t.games_played,
-    rank() OVER (ORDER BY t.gross_profit DESC) AS rank
-   FROM t;
-
-
-
-
 --
 -- TOC entry 2739 (class 2604 OID 87739)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
@@ -496,3 +472,24 @@ ALTER TABLE ONLY plays
 ALTER TABLE ONLY plays
     ADD CONSTRAINT plays_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
+
+CREATE OR REPLACE VIEW leaderboard AS 
+ WITH t AS (
+         SELECT u.id AS user_id,
+            u.username,
+            (COALESCE(sum(p.cash_out - p.bet), 0::numeric) + COALESCE(sum(p.bonus), 0::numeric))::bigint AS gross_profit,
+            (COALESCE(sum(p.cash_out), 0::numeric) + COALESCE(sum(p.bonus), 0::numeric) - COALESCE(sum(p.bet), 0::numeric))::bigint AS net_profit,
+            count(*) AS games_played
+           FROM plays p,
+            games g,
+            users u
+          WHERE p.game_id = g.id AND p.user_id = u.id AND p.id IS NOT NULL
+          GROUP BY u.id
+        )
+ SELECT t.user_id,
+    t.username,
+    t.gross_profit,
+    t.net_profit,
+    t.games_played,
+    rank() OVER (ORDER BY t.gross_profit DESC) AS rank
+   FROM t;
